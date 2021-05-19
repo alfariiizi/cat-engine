6e7b4e676de880eac664f224326cd23f26387e5e
alfariiizi <alfariiiziiiii@gmail.com>
2021-05-19 22:35:17 +0700

    newer cmake, seems more efficient and the structure is more neat than before

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,12 @@ int main()
         return EXIT_FAILURE;
     }
 
+    /// Uncomment and then hover these macros to check the values
+    // std::cout << SHADER_PATH << std::endl;
+    // std::cout << VERTEX_SHADER_PATH << std::endl;
+    // std::cout << FRAGMENT_SHADER_PATH << std::endl;
+    // std::cout << ASSETS_PATH << std::endl;
+
     std::cout << "Running Successfully\n";
 
     return EXIT_SUCCESS;
